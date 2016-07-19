1af71e5764f82f58180520d93a0b95b8d3409f77
roxm <rossymr@gmail.com>
2016-07-18 23:56:39 -0500

    REFACTOR & GREEN to advantage player 2
    1) Refactor isOnePlayerHavingAdvantage & getPlayerWithAdvantage to make
    pass Advantage player 1 and then
    2) Productive code to make pass Advantage for player 2

--- tennisGame.rb
+++ tennisGame.rb
@@ -15,13 +15,32 @@ class TennisGame
 	def score
 		if playerOneScore == 'forty' and playerTwoScore == 'forty'
 			return 'Duece'
-		elsif @playerOne.points == @playerTwo.points + 1 and @playerTwo.points >= 3
-			return "Advantage #{@playerOne.name}"
+		elsif isOnePlayerHavingAdvantage
+			return "Advantage #{getPlayerWithAdvantage}"
 		elsif @playerOne.points >= 4 and @playerTwo.points <= @playerOne.points - 2
 			return "Game to #{@playerOne.name}"
 		else		
 			return "Score #{playerOneScore}-#{playerTwoScore}"
 		end
 	end
+
+	def isOnePlayerHavingAdvantage
+		if @playerOne.points == @playerTwo.points + 1 and @playerTwo.points >= 3
+			return true
+		elsif @playerTwo.points == @playerOne.points + 1 and @playerOne.points >= 3
+			return true
+		else
+			return false
+		end
+	end
+
+	def getPlayerWithAdvantage
+		if isOnePlayerHavingAdvantage and @playerOne.points > @playerTwo.points then
+			return @playerOne.name
+		elsif isOnePlayerHavingAdvantage and @playerTwo.points > @playerOne.points then
+			return @playerTwo.name
+		end
+	end
+
 
 end
